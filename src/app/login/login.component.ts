d52cf9e50d6fc613d67765c9d3d2211a0cb376b1
juliusmlambo <jjdelisamlambo@gmail.com>
2020-09-28 18:40:59 +0200

    6 files modified, 1 deleted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,72 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-export class MyItems {    
-  Value: string;    
-  constructor(Value:string)    
-  {    
-    this.Value = Value;    
-  }    
-}    
-  
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit {
 
+  constructor() { }
 
-
-  myItems: MyItems[] = new Array();    
-    
-  // Other variables    
-  IsForUpdate: boolean = false;    
-  newItem: any = {};    
-  updatedItem;    
-    
-  // Provide some values to the array    
-  constructor()    
-  {    
-    this.myItems.push(    
-      new MyItems("First Value"),    
-      new MyItems("Second Value"),    
-      new MyItems("Third Value"),    
-      new MyItems("Forth Value"),    
-      new MyItems("Fifth Value")    
-    );    
-  } 
-
-  // To add new item in array    
- AddItem() {    
-   this.myItems.push(    
-     this.newItem    
-   );    
-   this.newItem = {};    
- }
-
- // When user selects edit option  
-EditItem(i) {  
-  this.newItem.Value = this.myItems[i].Value;  
-  this.updatedItem = i;  
-  this.IsForUpdate = true;  
-}  
-  
-// When user clicks on update button to submit updated value  
-UpdateItem() {  
-  let data = this.updatedItem;  
-  for (let i = 0; i < this.myItems.length; i++) {  
-    if (i == data) {  
-      this.myItems[i].Value = this.newItem.Value;  
-    }  
-  }  
-  this.IsForUpdate = false;  
-  this.newItem = {};  
-}
-
-// To delete specific item  
-DeleteItem(i) {  
-  this.myItems.splice(i, 1);  
-} 
+  ngOnInit() {
+  }
 
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,73 @@
+
 import { Component, OnInit } from '@angular/core';
 
+export class MyItems {    
+  Value: string;    
+  constructor(Value:string)    
+  {    
+    this.Value = Value;    
+  }    
+}    
+  
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
 
-  constructor() { }
+export class TodoListComponent{
 
-  ngOnInit() {
-  }
+
+
+  myItems: MyItems[] = new Array();    
+    
+  // Other variables    
+  IsForUpdate: boolean = false;    
+  newItem: any = {};    
+  updatedItem;    
+    
+  // Provide some values to the array    
+  constructor()    
+  {    
+    this.myItems.push(    
+      new MyItems("Develop ionic application"),    
+      new MyItems("Study"),    
+      new MyItems("Play FIFA"),    
+      new MyItems("Cook"),    
+      new MyItems("Sleep")    
+    );    
+  } 
+
+  // To add new item in array    
+ AddItem() {    
+   this.myItems.push(    
+     this.newItem    
+   );    
+   this.newItem = {};    
+ }
+
+ // When user selects edit option  
+EditItem(i) {  
+  this.newItem.Value = this.myItems[i].Value;  
+  this.updatedItem = i;  
+  this.IsForUpdate = true;  
+}  
+  
+// When user clicks on update button to submit updated value  
+UpdateItem() {  
+  let data = this.updatedItem;  
+  for (let i = 0; i < this.myItems.length; i++) {  
+    if (i == data) {  
+      this.myItems[i].Value = this.newItem.Value;  
+    }  
+  }  
+  this.IsForUpdate = false;  
+  this.newItem = {};  
+}
+
+// To delete specific item  
+DeleteItem(i) {  
+  this.myItems.splice(i, 1);  
+} 
 
 }
